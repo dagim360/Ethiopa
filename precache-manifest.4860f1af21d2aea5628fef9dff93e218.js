1a72f1c692a62f0d9645fd969ef6fa980488ae07
dagim360 <68782461+dagim360@users.noreply.github.com>
2022-02-06 02:28:16 -0800

    Updates

---
+++ precache-manifest.4860f1af21d2aea5628fef9dff93e218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe123e838a22d9d9d8a87e4bd91f1d3",
+    "url": "/Ethiopa/index.html"
+  },
+  {
+    "revision": "09e3c01d744025bf6134",
+    "url": "/Ethiopa/static/css/main.6cc9deb0.chunk.css"
+  },
+  {
+    "revision": "b8f60dc3a00e41ba604d",
+    "url": "/Ethiopa/static/js/2.eb3e42a2.chunk.js"
+  },
+  {
+    "revision": "44f515a80a48c76af8012084c85006fd",
+    "url": "/Ethiopa/static/js/2.eb3e42a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c71336508ab61d86aba",
+    "url": "/Ethiopa/static/js/3.bb6887e2.chunk.js"
+  },
+  {
+    "revision": "09e3c01d744025bf6134",
+    "url": "/Ethiopa/static/js/main.870d3b05.chunk.js"
+  },
+  {
+    "revision": "4db6d27a150a9f45e02b",
+    "url": "/Ethiopa/static/js/runtime-main.97ac4c79.js"
+  }
+]);
